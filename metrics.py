27f2d3b86dc5a91b45a68965ce85b839bdccb455
Peisong-Zhu <zhupeisong24@163.com>
2017-06-18 19:29:38 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,14 +1,5 @@
 import tensorflow as tf
 
-
-def masked_softmax_cross_entropy(labels, logits, mask):
-    """Softmax cross-entropy loss with masking."""
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
-    mask = tf.cast(mask, dtype=tf.float32)
-    mask /= tf.reduce_mean(mask)
-    loss *= mask
-    return tf.reduce_mean(loss)
-
 def masked_loss(loss, mask):
     """Softmax cross-entropy loss with masking."""
     mask = tf.cast(mask, dtype=tf.float32)
